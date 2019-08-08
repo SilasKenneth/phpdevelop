5233eaa995ed9d3115a1f6cba9abe8e8590640a4
Silas Kenneth <silaskenneth1@gmail.com>
2019-08-08 15:33:09 +0300

    Add basic tests

--- tests/students/StudentTest.php
+++ tests/students/StudentTest.php
@@ -1,7 +1,22 @@
 <?php
-
+$ROOT = realpath(dirname(__FILE__)."/../../");
+require_once "{$ROOT}/autoload.php";
+use PHPUnit\Framework\TestCase;
+use App\Controller\StudentController;
 
 class StudentTest extends TestCase
 {
+    private $controller;
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->controller = new StudentController();
+    }
+
+    public function testCanGetData(): void
+    {
+        print_r($this->controller->index());
+        $this->assertNotEquals($this->controller->index()->count(), 0);
+    }
 
 }
